0d9dd2e66d4a254c4c0fc1d046fb863330ddf65a
Lydia <srlee@maxst.com>
2023-01-06 17:55:56 +0900

    토픽 생성
    * new-topic : using NewTopic
    * new-topic.part1, new-topic.part2 : using KafkaAdmin.NewTopics
    * 토픽 생성 확인 방법 : docker-compose exec kafka kafka-topics.sh --list --zookeeper zookeeper:2181

---
+++ configuration/KafkaTopicConfiguration.java
@@ -0,0 +1,31 @@
+package com.sera.kafkademo.configuration;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+@Configuration
+public class KafkaTopicConfiguration {
+
+
+    @Bean
+    public NewTopic newTopic() {
+        return TopicBuilder.name("new-topic").build();
+    }
+
+    @Bean
+    public KafkaAdmin.NewTopics newTopics() {
+        return new KafkaAdmin.NewTopics(
+                TopicBuilder.name("new-topic.part1").build(),
+                TopicBuilder.name("new-topic.part2")
+                        .partitions(3)
+                        .replicas(1)
+                        .config(TopicConfig.RETENTION_MS_CONFIG, String.valueOf(1000 * 60 * 60)) // 1시간 Retention 을 가짐
+                        .build()
+        );
+    }
+}
