1943bd58d00d7577c9dc14917ed83e62fcea9c13
sera <cieroyou1@gmail.com>
2023-01-09 20:46:36 +0900

    publish message 추가
    * using CompletableFuture : ListenableFuture 는 Deprecated 됨

---
+++ src/main/java/com/sera/kafkademo/producer/NewTopicProducer.java
@@ -0,0 +1,37 @@
+package com.sera.kafkademo.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class NewTopicProducer {
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+
+    public void async(String topic, String message) {
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate
+                .send(topic, message);
+        future.whenComplete(((stringStringSendResult, throwable) -> {
+            if (throwable == null) {
+                System.out.println("successfully send");
+            } else {
+                System.out.println("exception occurred");
+            }
+        }));
+    }
+
+    public void sync(String topic, String message) {
+        try {
+            kafkaTemplate.send(topic, message);
+            System.out.println("successfully send");
+
+        } catch (Exception e) {
+            System.out.println("exception occurred");
+
+        }
+    }
+}
