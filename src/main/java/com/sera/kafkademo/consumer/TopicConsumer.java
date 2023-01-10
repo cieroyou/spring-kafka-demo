d43166c0a3c6d7546eb003f7798d9369ae42927a
Lydia <srlee@maxst.com>
2023-01-10 16:06:38 +0900

    필요없는 파일, 코드 삭제

--- src/main/java/com/sera/kafkademo/consumer/TopicConsumer.java
+++ src/main/java/com/sera/kafkademo/consumer/TopicConsumer.java
@@ -1,17 +1,8 @@
 package com.sera.kafkademo.consumer;
 
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicConsumer {
 
-
-    @KafkaListener(id = "new-topic-request-id", topics = "new-topic-request")
-    @SendTo
-    public String listenTopicRequest(String message) {
-        System.out.println(message);
-        return "Pong Topic";
-    }
 }
--- src/main/java/com/sera/kafkademo/producer/NewTopicProducer.java
+++ src/main/java/com/sera/kafkademo/producer/NewTopicProducer.java
@@ -1,31 +1,16 @@
 package com.sera.kafkademo.producer;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.RoutingKafkaTemplate;
-import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
-import org.springframework.kafka.requestreply.RequestReplyFuture;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 @Service
 public class NewTopicProducer {
     @Autowired
     KafkaTemplate<String, String> kafkaTemplate;
-
-    @Autowired
-    RoutingKafkaTemplate routingKafkaTemplate;
-
-    @Autowired
-    ReplyingKafkaTemplate<String, String, String> replyingKafkaTemplate;
-
 
     public void async(String topic, String message) {
         CompletableFuture<SendResult<String, String>> future = kafkaTemplate
@@ -39,26 +24,4 @@ public class NewTopicProducer {
         }));
     }
 
-    public void sync(String topic, String message) {
-        try {
-            kafkaTemplate.send(topic, message);
-            System.out.println("successfully send");
-
-        } catch (Exception e) {
-            System.out.println("exception occurred");
-
-        }
-    }
-
-    public void routingSend(String topic, String message) {
-        routingKafkaTemplate.send(topic, message);
-    }
-
-    public void replyingSend(String topic, String message) throws ExecutionException, InterruptedException, TimeoutException {
-        ProducerRecord<String, String> record = new ProducerRecord<>(topic, message);
-        RequestReplyFuture<String, String, String> replyFuture
-                = replyingKafkaTemplate.sendAndReceive(record);
-        ConsumerRecord<String, String> consumerRecord = replyFuture.get(10, TimeUnit.SECONDS);
-        System.out.println(consumerRecord.value());
-    }
 }
